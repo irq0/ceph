fce5e1b0a65c96538834d3212cc88fd22108be42
Jeff Layton <jlayton@redhat.com>
2021-04-15 13:04:38 -0400

    client: send fscrypt_auth and fscrypt_file in MClientRequest
    ...and update the fscrypt_file field on setattr changes for size.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    (cherry picked from commit 9e40efc3b9658a9d92b6249fd15f1d849fd6f465)

--- src/messages/MClientRequest.h
+++ src/messages/MClientRequest.h
@@ -67,7 +67,7 @@ WRITE_CLASS_ENCODER(SnapPayload)
 
 class MClientRequest final : public MMDSOp {
 private:
-  static constexpr int HEAD_VERSION = 5;
+  static constexpr int HEAD_VERSION = 6;
   static constexpr int COMPAT_VERSION = 1;
 
 public:
@@ -101,6 +101,9 @@ public:
   filepath path, path2;
   std::string alternate_name;
   std::vector<uint64_t> gid_list;
+
+  std::vector<uint8_t> fscrypt_auth;
+  std::vector<uint8_t> fscrypt_file;
 
   /* XXX HACK */
   mutable bool queued_for_replay = false;
@@ -244,6 +247,10 @@ public:
       decode(gid_list, p);
     if (header.version >= 5)
       decode(alternate_name, p);
+    if (header.version >= 6) {
+      decode(fscrypt_auth, p);
+      decode(fscrypt_file, p);
+    }
   }
 
   void encode_payload(uint64_t features) override {
@@ -276,6 +283,8 @@ public:
     encode(stamp, payload);
     encode(gid_list, payload);
     encode(alternate_name, payload);
+    encode(fscrypt_auth, payload);
+    encode(fscrypt_file, payload);
   }
 
   std::string_view get_type_name() const override { return "creq"; }
