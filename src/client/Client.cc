752625120714a18611e072289fa1bfd0f0117157
Jeff Layton <jlayton@redhat.com>
2021-04-29 13:33:53 -0400

    mds: convert fscrypt flag to two opaque fields
    A flag isn't sufficient as we can't reasonably use an xattr to store the
    context. Switch the fscrypt fields to two vectors of opaque bytes, one
    governed by AUTH caps and the other by FILE.
    
    Also remove the special handling for encryption.ctx xattr, since we
    won't be using that going forward anyway.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    (cherry picked from commit 1a593e1c3babf50a1c15161e348f617aabfceced)

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -1046,6 +1046,7 @@ Inode * Client::add_update_inode(InodeStat *st, utime_t from,
     in->btime = st->btime;
     in->snap_btime = st->snap_btime;
     in->snap_metadata = st->snap_metadata;
+    in->fscrypt_auth = st->fscrypt_auth;
     need_snapdir_attr_refresh = true;
   }
 
@@ -1063,6 +1064,7 @@ Inode * Client::add_update_inode(InodeStat *st, utime_t from,
   if (new_version ||
       (new_issued & (CEPH_CAP_ANY_FILE_RD | CEPH_CAP_ANY_FILE_WR))) {
     in->layout = st->layout;
+    in->fscrypt_file = st->fscrypt_file;
     update_inode_file_size(in, issued, st->size, st->truncate_seq, st->truncate_size);
   }
 
@@ -1146,7 +1148,6 @@ Inode * Client::add_update_inode(InodeStat *st, utime_t from,
     in->snap_caps |= st->cap.caps;
   }
 
-  in->fscrypt = st->fscrypt;
   if (need_snapdir_attr_refresh && in->is_dir() && in->snapid == CEPH_NOSNAP) {
     vinodeno_t vino(in->ino, CEPH_SNAPDIR);
     if (inode_map.count(vino)) {
