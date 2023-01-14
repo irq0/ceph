3a06292e746cfaa434b39386cfa578a611600779
Joao Eduardo Luis <joao@suse.com>
2023-01-14 16:51:34 +0000

    rgw/simplefile: Sqlite users using sqlite_orm library
    Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

--- src/rgw/rgw_sal_simplefile.h
+++ src/rgw/rgw_sal_simplefile.h
@@ -78,7 +78,6 @@ class UnsupportedLuaManager : public StoreLuaManager {
 
 class SimpleFileStore : public StoreDriver {
  private:
-  RGWUserInfo dummy_user;
   RGWSyncModuleInstanceRef sync_module;
   SimpleFileZone zone;
   const std::filesystem::path data_path;
