6dd92112c1972aa316eaa5852a255e3a601076a6
Marcel Lauhoff <marcel.lauhoff@suse.com>
2023-02-17 14:27:54 +0100

    sfs: fix whitespace errors and typos
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

--- src/rgw/driver/sfs/sqlite/dbconn.cc
+++ src/rgw/driver/sfs/sqlite/dbconn.cc
@@ -38,7 +38,7 @@ void DBConn::check_metadata_is_compatible(CephContext* ctt) {
     std::vector<std::string> non_compatible_tables;
     for (auto const& [table_name, sync_result] : sync_res) {
       if (sync_result == orm::sync_schema_result::dropped_and_recreated) {
-        // this result is aggresive as it drops the table and
+        // this result is aggressive as it drops the table and
         // recreates it.
         // Data loss is expected and we should warn the user and
         // stop the final sync in the real database.
@@ -47,11 +47,50 @@ void DBConn::check_metadata_is_compatible(CephContext* ctt) {
         non_compatible_tables.push_back(table_name);
       }
     }
-    if (non_compatible_tables.size() > 0) {
+
+    // try to sync the storage based on the temporary db
+    // in case something goes wrong show possible errors and return
+    auto test_storage = _make_storage(get_temporary_db_path(ctt));
+    test_storage.open_forever();
+    test_storage.busy_timeout(5000);
+    bool sync_error = false;
+    std::string result_message;
+    try {
+      auto sync_res = test_storage.sync_schema();
+      std::vector<std::string> non_compatible_tables;
+      for (auto const& [table_name, sync_result] : sync_res) {
+        if (sync_result == orm::sync_schema_result::dropped_and_recreated) {
+          // this result is aggressive as it drops the table and
+          // recreates it.
+          // Data loss is expected and we should warn the user and
+          // stop the final sync in the real database.
+          result_message +=
+              "Table: [" + table_name + "] is no longer compatible. ";
+          non_compatible_tables.push_back(table_name);
+        }
+      }
+      if (non_compatible_tables.size() > 0) {
+        sync_error = true;
+        result_message = "Tables: [ ";
+        for (auto const& table : non_compatible_tables) {
+          result_message += table + " ";
+        }
+        result_message += "] are no longer compatible.";
+      }
+    } catch (std::exception& e) {
+      // check for any other errors (foreign keys constrains, etc...)
+      result_message =
+          "Metadata database might be corrupted or is no longer compatible";
       sync_error = true;
-      result_message = "Tables: [ ";
-      for (auto const& table : non_compatible_tables) {
-        result_message += table + " ";
+    }
+    // remove the temporary db
+    fs::remove(get_temporary_db_path(ctt));
+
+    try {
+      fs::remove(get_temporary_db_path(ctt) + "-wal");
+    } catch (const std::filesystem::filesystem_error& e) {
+      if (e.code() != std::errc::no_such_file_or_directory) {
+        throw e;
       }
       result_message += "] are no longer compatible.";
     }
