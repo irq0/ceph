wipwd/s3gw-ceph

rgw/sfs: add basic write support
  note: contains transient code.

Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/store/simplefile/bucket.h
@@ -18,7 +18,10 @@
 #include <string>
 #include <memory>
 
+#include "common/Formatter.h"
+#include "common/ceph_json.h"
 #include "rgw_sal.h"
+#include "store/simplefile/multipart.h"
 
 
 namespace rgw::sal {
@@ -30,8 +33,33 @@ class SimpleFileBucket : public Bucket {
   SimpleFileStore *store;
   const std::filesystem::path path;
   RGWAccessControlPolicy acls;
+  // std::map<std::string, SimpleFileMultipartUpload> multipart;
  protected:
+
+  class Meta {
+   public:
+    RGWBucketInfo info;
+    std::set<std::string> multipart;
+
+    void dump(ceph::Formatter *f) const {
+      f->open_object_section("info");
+      info.dump(f);
+      f->close_section();  // info
+      // encode_json("multipart", multipart, f);
+    }
+
+    void decode_json(JSONObj *obj) {
+      JSONDecoder::decode_json("info", info, obj);
+      // JSONDecoder::decode_json("multipart", multipart, obj);
+    }
+  };
+
   SimpleFileBucket(const SimpleFileBucket&) = default;
+
+  void write_meta(const DoutPrefixProvider *dpp);
+  // void read_meta(const DoutPrefixProvider *dpp);
+  // void add_multipart(const std::string &oid, const std::string &meta);
+  // void remove_multipart(const std::string &oid, const std::string &meta);
 
  public:
   SimpleFileBucket(
@@ -139,6 +167,12 @@ class SimpleFileBucket : public Bucket {
   virtual int purge_instance(const DoutPrefixProvider *dpp) override {
     return -ENOTSUP;
   }
+
+  void mark_multipart_complete(
+    const DoutPrefixProvider *dpp,
+    const std::string &oid,
+    const std::string &meta
+  );
   inline std::string get_cls_name() { return "bucket"; }
 };
 

src/rgw/store/simplefile/multipart.h
@@ -1,0 +1,204 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SIMPLEFILE_MULTIPART_H
+#define RGW_STORE_SIMPLEFILE_MULTIPART_H
+
+#include "rgw_sal.h"
+
+namespace rgw::sal {
+
+class SimpleFileStore;
+
+class SimpleFileMultipartMeta {
+
+  ACLOwner owner;
+  Attrs attrs;
+  rgw_placement_rule dest_placement;
+  RGWObjCategory category;
+  int flags;
+  ceph::real_time mtime;
+
+ public:
+  SimpleFileMultipartMeta(
+    ACLOwner &_owner,
+    Attrs &_attrs,
+    rgw_placement_rule &_dest_placement,
+    RGWObjCategory _category,
+    int _flags,
+    ceph::real_time &_mtime
+  ) : owner(_owner), attrs(_attrs), dest_placement(_dest_placement),
+      category(_category), flags(_flags), mtime(_mtime) { }
+
+  void encode(bufferlist &bl) const {
+    ENCODE_START(1, 1, bl);
+    encode(owner, bl);
+    encode(attrs, bl);
+    encode(dest_placement, bl);
+    encode(flags, bl);
+    encode(mtime, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::const_iterator &bl) {
+    DECODE_START(1, bl);
+    decode(owner, bl);
+    decode(attrs, bl);
+    decode(dest_placement, bl);
+    decode(flags, bl);
+    decode(mtime, bl);
+    DECODE_FINISH(bl);
+  }
+
+};
+WRITE_CLASS_ENCODER(SimpleFileMultipartMeta)
+
+class SimpleFileMultipartObject {
+
+  std::string oid;  // object id / name
+  std::string upload_id;
+  std::string meta;
+
+  std::string gen_upload_id();
+  void init(CephContext *cct, std::string _oid, std::string _upload_id);
+
+ public:
+  SimpleFileMultipartObject(
+    CephContext *cct,
+    const std::string &_oid,
+    const std::string &_upload_id
+  );
+  SimpleFileMultipartObject(
+    CephContext *cct,
+    const std::string &_oid,
+    const std::optional<std::string> _upload_id
+  );
+
+  const std::string& get_key() const {
+    return oid;
+  }
+
+  const std::string& get_upload_id() const {
+    return upload_id;
+  }
+
+  const std::string& get_meta() const {
+    return meta;
+  }
+
+  friend inline std::ostream& operator<<(
+    std::ostream &out, const SimpleFileMultipartObject &obj
+  ) {
+    out << "multipart_object(oid: " << obj.get_key() << ", upload_id: "
+        << obj.get_upload_id() << ", meta: " << obj.get_meta() << ")";
+    return out;
+  }
+
+};
+
+class SimpleFileMultipartUpload : public MultipartUpload {
+
+  SimpleFileStore *store;
+  const std::string &oid;
+  std::optional<std::string> upload_id;
+  SimpleFileMultipartObject obj;
+  ACLOwner owner;
+  ceph::real_time mtime;
+
+ public:
+  SimpleFileMultipartUpload(
+    CephContext *_cct,
+    SimpleFileStore *_store,
+    Bucket *_bucket,
+    const std::string &_oid,
+    std::optional<std::string> _upload_id,
+    ACLOwner _owner,
+    ceph::real_time _mtime
+  ) : MultipartUpload(_bucket), store(_store),
+      oid(_oid), upload_id(_upload_id),
+      obj(_cct, _oid, _upload_id),
+      owner(_owner), mtime(_mtime) { }
+
+  virtual ~SimpleFileMultipartUpload() = default;
+
+  virtual const std::string& get_meta() const {
+    return obj.get_meta();
+  }
+  virtual const std::string& get_key() const {
+    return obj.get_key();
+  }
+  virtual const std::string& get_upload_id() const {
+    return obj.get_upload_id();
+  }
+  virtual const ACLOwner& get_owner() const override { return owner; }
+  virtual ceph::real_time& get_mtime() { return mtime; }
+  virtual std::unique_ptr<rgw::sal::Object> get_meta_obj() override;
+  virtual int init(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    ACLOwner &owner,
+    rgw_placement_rule &dest_placement,
+    rgw::sal::Attrs &attrs
+  ) override;
+  virtual int list_parts(
+    const DoutPrefixProvider *dpp,
+    CephContext *cct,
+    int num_parts,
+    int marker,
+    int *next_marker,
+    bool *truncated,
+    bool assume_unsorted = false
+  ) override;
+  virtual int abort(const DoutPrefixProvider *dpp, CephContext *cct) override;
+  virtual int complete(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    CephContext *cct,
+    std::map<int, std::string> &part_etags,
+    std::list<rgw_obj_index_key> &remove_objs,
+    uint64_t &accounted_size,
+    bool &compressed,
+    RGWCompressionInfo &cs_info,
+    off_t &ofs,
+    std::string &tag,
+    ACLOwner &owner,
+    uint64_t olh_epoch,
+    rgw::sal::Object *target_obj
+  ) override;
+  virtual int get_info(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    rgw_placement_rule **rule,
+    rgw::sal::Attrs *attrs = nullptr
+  ) override;
+  virtual std::unique_ptr<Writer> get_writer(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    std::unique_ptr<rgw::sal::Object> head_obj,
+    const rgw_user &owner,
+    const rgw_placement_rule *ptail_placement_rule,
+    uint64_t part_num,
+    const std::string &part_num_str
+  ) override;
+
+  void dump(Formatter *f) const;
+  int write_metadata(
+    const DoutPrefixProvider *dpp,
+    SimpleFileMultipartMeta &metadata
+  );
+  inline std::string get_cls_name() { return "multipart_upload"; }
+};
+
+} // ns rgw::sal
+
+#endif // RGW_STORE_SIMPLEFILE_MULTIPART_H

src/rgw/store/simplefile/writer.cc
@@ -1,0 +1,113 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#include <memory>
+#include "rgw_sal.h"
+#include "rgw_sal_simplefile.h"
+#include "store/simplefile/writer.h"
+
+#define dout_subsys ceph_subsys_rgw
+
+using namespace std;
+
+namespace rgw::sal {
+
+SimpleFileAtomicWriter::SimpleFileAtomicWriter(
+  const DoutPrefixProvider *_dpp,
+  optional_yield _y,
+  std::unique_ptr<rgw::sal::Object> _head_obj,
+  SimpleFileStore *_store,
+  const rgw_user& _owner,
+  const rgw_placement_rule *_ptail_placement_rule,
+  uint64_t _olh_epoch,
+  const std::string &_unique_tag
+) :
+  Writer(_dpp, _y) { }
+
+int SimpleFileAtomicWriter::prepare(optional_yield y) {
+  ldpp_dout(dpp, 10) << __func__ << ": unimplemented, return success." << dendl;
+  return 0;
+}
+
+int SimpleFileAtomicWriter::process(bufferlist &&data, uint64_t offset) {
+  ldpp_dout(dpp, 10) << "atomic_writer::" << __func__ << ": data len: "
+                     << data.length() << ", offset: " << offset << dendl;
+  ldpp_dout(dpp, 10) << "atomic_writer::" << __func__
+                     << ": unimplemented, return success." << dendl;
+  return 0;
+}
+
+int SimpleFileAtomicWriter::complete(
+  size_t accounted_size,
+  const std::string &etag,
+  ceph::real_time *mtime,
+  ceph::real_time set_mtime,
+  std::map<std::string, bufferlist> &attrs,
+  ceph::real_time delete_at,
+  const char *if_match,
+  const char *if_nomatch,
+  const std::string *user_data,
+  rgw_zone_set *zones_trace,
+  bool *canceled,
+  optional_yield y
+) {
+  ldpp_dout(dpp, 10) << __func__ << ": unimplemented, return success." << dendl;
+  return 0;
+}
+
+SimpleFileMultipartWriter::SimpleFileMultipartWriter(
+  const DoutPrefixProvider *_dpp,
+  optional_yield y,
+  MultipartUpload *upload,
+  std::unique_ptr<rgw::sal::Object> _head_obj,
+  const SimpleFileStore *store,
+  const rgw_user &_owner,
+  const rgw_placement_rule *_ptail_placement_rule,
+  uint64_t _part_num,
+  const std::string &_part_num_str
+) : Writer(_dpp, y) { }
+
+int SimpleFileMultipartWriter::prepare(optional_yield y) {
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": unimplemented, return success" << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartWriter::process(bufferlist &&data, uint64_t offset) {
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": unimplemented, return success" << dendl;
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": data len: " << data.length() << ", offset: " << offset << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartWriter::complete(
+  size_t accounted_size,
+  const std::string &etag,
+  ceph::real_time *mtime,
+  ceph::real_time set_mtime,
+  std::map<std::string, bufferlist> &attrs,
+  ceph::real_time delete_at,
+  const char *if_match,
+  const char *if_nomatch,
+  const std::string *user_data,
+  rgw_zone_set *zones_trace,
+  bool *canceled,
+  optional_yield y
+) {
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": unimplemented, return success" << dendl;
+  return 0;
+}
+
+} // ns rgw::sal

src/rgw/store/simplefile/writer.h
@@ -1,0 +1,101 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SIMPLEFILE_WRITER_H
+#define RGW_STORE_SIMPLEFILE_WRITER_H
+
+#include <memory>
+#include "rgw_sal.h"
+
+namespace rgw::sal {
+
+class SimpleFileStore;
+
+class SimpleFileAtomicWriter : public Writer {
+
+ protected:
+  rgw::sal::SimpleFileStore *store;
+
+ public:
+  SimpleFileAtomicWriter(
+    const DoutPrefixProvider *_dpp,
+    optional_yield _y,
+    std::unique_ptr<rgw::sal::Object> _head_obj,
+    SimpleFileStore *_store,
+    const rgw_user& _owner,
+    const rgw_placement_rule *_ptail_placement_rule,
+    uint64_t _olh_epoch,
+    const std::string &_unique_tag
+  );
+  ~SimpleFileAtomicWriter() = default;
+
+  virtual int prepare(optional_yield y) override;
+  virtual int process(bufferlist &&data, uint64_t offset) override;
+  virtual int complete(
+    size_t accounted_size,
+    const std::string &etag,
+    ceph::real_time *mtime,
+    ceph::real_time set_mtime,
+    std::map<std::string, bufferlist> &attrs,
+    ceph::real_time delete_at,
+    const char *if_match,
+    const char *if_nomatch,
+    const std::string *user_data,
+    rgw_zone_set *zones_trace,
+    bool *canceled,
+    optional_yield y
+  ) override;
+
+};
+
+class SimpleFileMultipartWriter : public Writer {
+
+ protected:
+  const rgw::sal::SimpleFileStore *store;
+
+ public:
+  SimpleFileMultipartWriter(
+    const DoutPrefixProvider *_dpp,
+    optional_yield y,
+    MultipartUpload *upload,
+    std::unique_ptr<rgw::sal::Object> _head_obj,
+    const SimpleFileStore *store,
+    const rgw_user &_owner,
+    const rgw_placement_rule *_ptail_placement_rule,
+    uint64_t _part_num,
+    const std::string &_part_num_str
+  );
+  ~SimpleFileMultipartWriter() = default;
+
+  virtual int prepare(optional_yield y) override;
+  virtual int process(bufferlist &&data, uint64_t offset) override;
+  virtual int complete(
+    size_t accounted_size,
+    const std::string &etag,
+    ceph::real_time *mtime,
+    ceph::real_time set_mtime,
+    std::map<std::string, bufferlist> &attrs,
+    ceph::real_time delete_at,
+    const char *if_match,
+    const char *if_nomatch,
+    const std::string *user_data,
+    rgw_zone_set *zones_trace,
+    bool *canceled,
+    optional_yield y
+  ) override;
+
+};
+
+} // ns rgw::sal
+
+#endif // RGW_STORE_SIMPLEFILE_WRITER_H

src/rgw/rgw_sal_simplefile.cc
@@ -31,6 +31,8 @@
 #include "services/svc_zone_utils.h"
 
 #include "store/simplefile/notification.h"
+#include "store/simplefile/writer.h"
+
 #define dout_subsys ceph_subsys_rgw
 
 using namespace std;
@@ -97,12 +99,20 @@ std::unique_ptr<Writer> SimpleFileStore::get_append_writer(
 }
 /** Get a Writer that atomically writes an entire object */
 std::unique_ptr<Writer> SimpleFileStore::get_atomic_writer(
-    const DoutPrefixProvider *dpp, optional_yield y,
-    std::unique_ptr<rgw::sal::Object> _head_obj, const rgw_user &owner,
-    const rgw_placement_rule *ptail_placement_rule, uint64_t olh_epoch,
-    const std::string &unique_tag) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return nullptr;
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    std::unique_ptr<rgw::sal::Object> _head_obj,
+    const rgw_user &owner,
+    const rgw_placement_rule *ptail_placement_rule,
+    uint64_t olh_epoch,
+    const std::string &unique_tag
+) {
+  ldpp_dout(dpp, 10) << __func__ << ": return basic atomic writer" << dendl;
+  return std::make_unique<SimpleFileAtomicWriter>(
+      dpp, y, std::move(_head_obj), this,
+      owner, ptail_placement_rule,
+      olh_epoch, unique_tag
+  );
 }
 
 // }}}

irq0/ceph

src/rgw/driver/simplefile/bucket.h
@@ -18,8 +18,11 @@
 #include <string>
 #include <memory>
 
+#include "common/Formatter.h"
+#include "common/ceph_json.h"
 #include "rgw_sal.h"
 #include "rgw_sal_store.h"
+#include "driver/simplefile/multipart.h"
 
 
 namespace rgw::sal {
@@ -30,8 +33,33 @@ class SimpleFileBucket : public StoreBucket {
   SimpleFileStore *store;
   const std::filesystem::path path;
   RGWAccessControlPolicy acls;
+  // std::map<std::string, SimpleFileMultipartUpload> multipart;
  protected:
+
+  class Meta {
+   public:
+    RGWBucketInfo info;
+    std::set<std::string> multipart;
+
+    void dump(ceph::Formatter *f) const {
+      f->open_object_section("info");
+      info.dump(f);
+      f->close_section();  // info
+      // encode_json("multipart", multipart, f);
+    }
+
+    void decode_json(JSONObj *obj) {
+      JSONDecoder::decode_json("info", info, obj);
+      // JSONDecoder::decode_json("multipart", multipart, obj);
+    }
+  };
+
   SimpleFileBucket(const SimpleFileBucket&) = default;
+
+  void write_meta(const DoutPrefixProvider *dpp);
+  // void read_meta(const DoutPrefixProvider *dpp);
+  // void add_multipart(const std::string &oid, const std::string &meta);
+  // void remove_multipart(const std::string &oid, const std::string &meta);
 
  public:
   SimpleFileBucket(
@@ -150,6 +178,12 @@ class SimpleFileBucket : public StoreBucket {
   virtual int purge_instance(const DoutPrefixProvider *dpp) override {
     return -ENOTSUP;
   }
+
+  void mark_multipart_complete(
+    const DoutPrefixProvider *dpp,
+    const std::string &oid,
+    const std::string &meta
+  );
   inline std::string get_cls_name() { return "bucket"; }
 };
 

src/rgw/driver/simplefile/multipart.h
@@ -1,0 +1,205 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SIMPLEFILE_MULTIPART_H
+#define RGW_STORE_SIMPLEFILE_MULTIPART_H
+
+#include "rgw_sal.h"
+#include "rgw_sal_store.h"
+
+namespace rgw::sal {
+
+class SimpleFileStore;
+
+class SimpleFileMultipartMeta {
+
+  ACLOwner owner;
+  Attrs attrs;
+  rgw_placement_rule dest_placement;
+  RGWObjCategory category;
+  int flags;
+  ceph::real_time mtime;
+
+ public:
+  SimpleFileMultipartMeta(
+    ACLOwner &_owner,
+    Attrs &_attrs,
+    rgw_placement_rule &_dest_placement,
+    RGWObjCategory _category,
+    int _flags,
+    ceph::real_time &_mtime
+  ) : owner(_owner), attrs(_attrs), dest_placement(_dest_placement),
+      category(_category), flags(_flags), mtime(_mtime) { }
+
+  void encode(bufferlist &bl) const {
+    ENCODE_START(1, 1, bl);
+    encode(owner, bl);
+    encode(attrs, bl);
+    encode(dest_placement, bl);
+    encode(flags, bl);
+    encode(mtime, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::const_iterator &bl) {
+    DECODE_START(1, bl);
+    decode(owner, bl);
+    decode(attrs, bl);
+    decode(dest_placement, bl);
+    decode(flags, bl);
+    decode(mtime, bl);
+    DECODE_FINISH(bl);
+  }
+
+};
+WRITE_CLASS_ENCODER(SimpleFileMultipartMeta)
+
+class SimpleFileMultipartObject {
+
+  std::string oid;  // object id / name
+  std::string upload_id;
+  std::string meta;
+
+  std::string gen_upload_id();
+  void init(CephContext *cct, std::string _oid, std::string _upload_id);
+
+ public:
+  SimpleFileMultipartObject(
+    CephContext *cct,
+    const std::string &_oid,
+    const std::string &_upload_id
+  );
+  SimpleFileMultipartObject(
+    CephContext *cct,
+    const std::string &_oid,
+    const std::optional<std::string> _upload_id
+  );
+
+  const std::string& get_key() const {
+    return oid;
+  }
+
+  const std::string& get_upload_id() const {
+    return upload_id;
+  }
+
+  const std::string& get_meta() const {
+    return meta;
+  }
+
+  friend inline std::ostream& operator<<(
+    std::ostream &out, const SimpleFileMultipartObject &obj
+  ) {
+    out << "multipart_object(oid: " << obj.get_key() << ", upload_id: "
+        << obj.get_upload_id() << ", meta: " << obj.get_meta() << ")";
+    return out;
+  }
+
+};
+
+class SimpleFileMultipartUpload : public StoreMultipartUpload {
+
+  SimpleFileStore *store;
+  const std::string &oid;
+  std::optional<std::string> upload_id;
+  SimpleFileMultipartObject obj;
+  ACLOwner owner;
+  ceph::real_time mtime;
+
+ public:
+  SimpleFileMultipartUpload(
+    CephContext *_cct,
+    SimpleFileStore *_store,
+    Bucket *_bucket,
+    const std::string &_oid,
+    std::optional<std::string> _upload_id,
+    ACLOwner _owner,
+    ceph::real_time _mtime
+  ) : StoreMultipartUpload(_bucket), store(_store),
+      oid(_oid), upload_id(_upload_id),
+      obj(_cct, _oid, _upload_id),
+      owner(_owner), mtime(_mtime) { }
+
+  virtual ~SimpleFileMultipartUpload() = default;
+
+  virtual const std::string& get_meta() const {
+    return obj.get_meta();
+  }
+  virtual const std::string& get_key() const {
+    return obj.get_key();
+  }
+  virtual const std::string& get_upload_id() const {
+    return obj.get_upload_id();
+  }
+  virtual const ACLOwner& get_owner() const override { return owner; }
+  virtual ceph::real_time& get_mtime() { return mtime; }
+  virtual std::unique_ptr<rgw::sal::Object> get_meta_obj() override;
+  virtual int init(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    ACLOwner &owner,
+    rgw_placement_rule &dest_placement,
+    rgw::sal::Attrs &attrs
+  ) override;
+  virtual int list_parts(
+    const DoutPrefixProvider *dpp,
+    CephContext *cct,
+    int num_parts,
+    int marker,
+    int *next_marker,
+    bool *truncated,
+    bool assume_unsorted = false
+  ) override;
+  virtual int abort(const DoutPrefixProvider *dpp, CephContext *cct) override;
+  virtual int complete(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    CephContext *cct,
+    std::map<int, std::string> &part_etags,
+    std::list<rgw_obj_index_key> &remove_objs,
+    uint64_t &accounted_size,
+    bool &compressed,
+    RGWCompressionInfo &cs_info,
+    off_t &ofs,
+    std::string &tag,
+    ACLOwner &owner,
+    uint64_t olh_epoch,
+    rgw::sal::Object *target_obj
+  ) override;
+  virtual int get_info(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    rgw_placement_rule **rule,
+    rgw::sal::Attrs *attrs = nullptr
+  ) override;
+  virtual std::unique_ptr<Writer> get_writer(
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    rgw::sal::Object* head_obj,
+    const rgw_user &owner,
+    const rgw_placement_rule *ptail_placement_rule,
+    uint64_t part_num,
+    const std::string &part_num_str
+  ) override;
+
+  void dump(Formatter *f) const;
+  int write_metadata(
+    const DoutPrefixProvider *dpp,
+    SimpleFileMultipartMeta &metadata
+  );
+  inline std::string get_cls_name() { return "multipart_upload"; }
+};
+
+} // ns rgw::sal
+
+#endif // RGW_STORE_SIMPLEFILE_MULTIPART_H

src/rgw/driver/simplefile/writer.cc
@@ -1,0 +1,113 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#include <memory>
+#include "rgw_sal.h"
+#include "rgw_sal_simplefile.h"
+#include "driver/simplefile/writer.h"
+
+#define dout_subsys ceph_subsys_rgw
+
+using namespace std;
+
+namespace rgw::sal {
+
+SimpleFileAtomicWriter::SimpleFileAtomicWriter(
+  const DoutPrefixProvider *_dpp,
+  optional_yield _y,
+  rgw::sal::Object* _head_obj,
+  SimpleFileStore *_store,
+  const rgw_user& _owner,
+  const rgw_placement_rule *_ptail_placement_rule,
+  uint64_t _olh_epoch,
+  const std::string &_unique_tag
+) :
+  StoreWriter(_dpp, _y) { }
+
+int SimpleFileAtomicWriter::prepare(optional_yield y) {
+  ldpp_dout(dpp, 10) << __func__ << ": unimplemented, return success." << dendl;
+  return 0;
+}
+
+int SimpleFileAtomicWriter::process(bufferlist &&data, uint64_t offset) {
+  ldpp_dout(dpp, 10) << "atomic_writer::" << __func__ << ": data len: "
+                     << data.length() << ", offset: " << offset << dendl;
+  ldpp_dout(dpp, 10) << "atomic_writer::" << __func__
+                     << ": unimplemented, return success." << dendl;
+  return 0;
+}
+
+int SimpleFileAtomicWriter::complete(
+  size_t accounted_size,
+  const std::string &etag,
+  ceph::real_time *mtime,
+  ceph::real_time set_mtime,
+  std::map<std::string, bufferlist> &attrs,
+  ceph::real_time delete_at,
+  const char *if_match,
+  const char *if_nomatch,
+  const std::string *user_data,
+  rgw_zone_set *zones_trace,
+  bool *canceled,
+  optional_yield y
+) {
+  ldpp_dout(dpp, 10) << __func__ << ": unimplemented, return success." << dendl;
+  return 0;
+}
+
+SimpleFileMultipartWriter::SimpleFileMultipartWriter(
+  const DoutPrefixProvider *_dpp,
+  optional_yield y,
+  MultipartUpload *upload,
+  rgw::sal::Object* _head_obj,
+  const SimpleFileStore *store,
+  const rgw_user &_owner,
+  const rgw_placement_rule *_ptail_placement_rule,
+  uint64_t _part_num,
+  const std::string &_part_num_str
+) : StoreWriter(_dpp, y) { }
+
+int SimpleFileMultipartWriter::prepare(optional_yield y) {
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": unimplemented, return success" << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartWriter::process(bufferlist &&data, uint64_t offset) {
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": unimplemented, return success" << dendl;
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": data len: " << data.length() << ", offset: " << offset << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartWriter::complete(
+  size_t accounted_size,
+  const std::string &etag,
+  ceph::real_time *mtime,
+  ceph::real_time set_mtime,
+  std::map<std::string, bufferlist> &attrs,
+  ceph::real_time delete_at,
+  const char *if_match,
+  const char *if_nomatch,
+  const std::string *user_data,
+  rgw_zone_set *zones_trace,
+  bool *canceled,
+  optional_yield y
+) {
+  ldpp_dout(dpp, 10) << "multipart_writer::" << __func__
+                     << ": unimplemented, return success" << dendl;
+  return 0;
+}
+
+} // ns rgw::sal

src/rgw/driver/simplefile/writer.h
@@ -1,0 +1,102 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SIMPLEFILE_WRITER_H
+#define RGW_STORE_SIMPLEFILE_WRITER_H
+
+#include <memory>
+#include "rgw_sal.h"
+#include "rgw_sal_store.h"
+
+namespace rgw::sal {
+
+class SimpleFileStore;
+
+class SimpleFileAtomicWriter : public StoreWriter {
+
+ protected:
+  rgw::sal::SimpleFileStore *store;
+
+ public:
+  SimpleFileAtomicWriter(
+    const DoutPrefixProvider *_dpp,
+    optional_yield _y,
+    rgw::sal::Object* _head_obj,
+    SimpleFileStore *_store,
+    const rgw_user& _owner,
+    const rgw_placement_rule *_ptail_placement_rule,
+    uint64_t _olh_epoch,
+    const std::string &_unique_tag
+  );
+  ~SimpleFileAtomicWriter() = default;
+
+  virtual int prepare(optional_yield y) override;
+  virtual int process(bufferlist &&data, uint64_t offset) override;
+  virtual int complete(
+    size_t accounted_size,
+    const std::string &etag,
+    ceph::real_time *mtime,
+    ceph::real_time set_mtime,
+    std::map<std::string, bufferlist> &attrs,
+    ceph::real_time delete_at,
+    const char *if_match,
+    const char *if_nomatch,
+    const std::string *user_data,
+    rgw_zone_set *zones_trace,
+    bool *canceled,
+    optional_yield y
+  ) override;
+
+};
+
+class SimpleFileMultipartWriter : public StoreWriter {
+
+ protected:
+  const rgw::sal::SimpleFileStore *store;
+
+ public:
+  SimpleFileMultipartWriter(
+    const DoutPrefixProvider *_dpp,
+    optional_yield y,
+    MultipartUpload *upload,
+    rgw::sal::Object* _head_obj,
+    const SimpleFileStore *store,
+    const rgw_user &_owner,
+    const rgw_placement_rule *_ptail_placement_rule,
+    uint64_t _part_num,
+    const std::string &_part_num_str
+  );
+  ~SimpleFileMultipartWriter() = default;
+
+  virtual int prepare(optional_yield y) override;
+  virtual int process(bufferlist &&data, uint64_t offset) override;
+  virtual int complete(
+    size_t accounted_size,
+    const std::string &etag,
+    ceph::real_time *mtime,
+    ceph::real_time set_mtime,
+    std::map<std::string, bufferlist> &attrs,
+    ceph::real_time delete_at,
+    const char *if_match,
+    const char *if_nomatch,
+    const std::string *user_data,
+    rgw_zone_set *zones_trace,
+    bool *canceled,
+    optional_yield y
+  ) override;
+
+};
+
+} // ns rgw::sal
+
+#endif // RGW_STORE_SIMPLEFILE_WRITER_H

src/rgw/rgw_sal_simplefile.cc
@@ -31,6 +31,8 @@
 #include "services/svc_zone_utils.h"
 
 #include "driver/simplefile/notification.h"
+#include "driver/simplefile/writer.h"
+
 #define dout_subsys ceph_subsys_rgw
 
 using namespace std;
@@ -91,12 +93,20 @@ std::unique_ptr<Writer> SimpleFileStore::get_append_writer(
 }
 /** Get a Writer that atomically writes an entire object */
 std::unique_ptr<Writer> SimpleFileStore::get_atomic_writer(
-    const DoutPrefixProvider *dpp, optional_yield y,
-    rgw::sal::Object* _head_obj, const rgw_user &owner,
-    const rgw_placement_rule *ptail_placement_rule, uint64_t olh_epoch,
-    const std::string &unique_tag) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return nullptr;
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    rgw::sal::Object* _head_obj,
+    const rgw_user &owner,
+    const rgw_placement_rule *ptail_placement_rule,
+    uint64_t olh_epoch,
+    const std::string &unique_tag
+) {
+  ldpp_dout(dpp, 10) << __func__ << ": return basic atomic writer" << dendl;
+  return std::make_unique<SimpleFileAtomicWriter>(
+      dpp, y, _head_obj, this,
+      owner, ptail_placement_rule,
+      olh_epoch, unique_tag
+  );
 }
 
 // }}}