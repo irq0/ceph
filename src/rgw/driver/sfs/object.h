wipwd/s3gw-ceph

rgw/sfs: move meta to memory, new data layout
This commit introduces a new on-disk data layout, where objects are kept
in a UUID-driven hash tree. We also drop all metadata from disk and keep
it in memory, laying the path to introducing sqlite-backed metadata.

Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/store/sfs/object.h
@@ -14,7 +14,13 @@
 #ifndef RGW_STORE_SFS_OBJECT_H
 #define RGW_STORE_SFS_OBJECT_H
 
+#include <filesystem>
+
 #include "rgw_sal.h"
+#include "uuid_path.h"
+#include "rgw/store/sfs/bucket.h"
+#include "rgw/store/sfs/types.h"
+
 
 namespace rgw::sal {
 
@@ -24,44 +30,12 @@ class SFSObject : public Object {
  private:
   SFStore *store;
   RGWAccessControlPolicy acls;
+  sfs::BucketRef bucketref;
+  sfs::ObjectRef objref;
  protected:
   SFSObject(SFSObject&) = default;
-  void init() {
-    load_meta();
-  }
 
  public:
-
-  struct Meta {
-    size_t size;
-    std::string etag;
-    ceph::real_time mtime;
-    ceph::real_time set_mtime;
-    ceph::real_time delete_at;
-    std::map<std::string, bufferlist> attrs;
-
-    Meta() { }
-    ~Meta() = default;
-
-    void dump(Formatter *f) const {
-      encode_json("size", size, f);
-      encode_json("etag", etag, f);
-      encode_json("mtime", mtime, f);
-      encode_json("set_mtime", set_mtime, f);
-      encode_json("delete_at", delete_at, f);
-      encode_json("attrs", attrs, f);
-    }
-
-    void decode_json(JSONObj *obj) {
-      JSONDecoder::decode_json("size", size, obj);
-      JSONDecoder::decode_json("etag", etag, obj);
-      JSONDecoder::decode_json("mtime", mtime, obj);
-      JSONDecoder::decode_json("set_mtime", set_mtime, obj);
-      JSONDecoder::decode_json("delete_at", delete_at, obj);
-      JSONDecoder::decode_json("attrs", attrs, obj);
-    }
-  };
-  SFSObject::Meta meta;
 
   /**
    * reads an object's contents.
@@ -69,10 +43,11 @@ class SFSObject : public Object {
   struct SFSReadOp : public ReadOp {
    private:
     SFSObject *source;
-    RGWObjectCtx *rctx;
+    sfs::ObjectRef objref;
+    std::filesystem::path objdata;
 
    public:
-    SFSReadOp(SFSObject *_source, RGWObjectCtx *_rctx);
+    SFSReadOp(SFSObject *_source);
 
     virtual int prepare(optional_yield y,
                         const DoutPrefixProvider *dpp) override;
@@ -92,10 +67,10 @@ class SFSObject : public Object {
   struct SFSDeleteOp : public DeleteOp {
    private:
     SFSObject *source;
-    BucketMgrRef mgr;
+    sfs::BucketRef bucketref;
 
    public:
-    SFSDeleteOp(SFSObject *_source, BucketMgrRef _mgr);
+    SFSDeleteOp(SFSObject *_source, sfs::BucketRef _bucketref);
     virtual int delete_obj(const DoutPrefixProvider *dpp,
                            optional_yield y) override;
 
@@ -112,9 +87,10 @@ class SFSObject : public Object {
   SFSObject(
     SFStore *_st,
     const rgw_obj_key &_k,
-    Bucket *_b
-  ) : Object(_k, _b), store(_st) {
-    init();
+    Bucket *_b,
+    sfs::BucketRef _bucket
+  ) : Object(_k, _b), store(_st), bucketref(_bucket) {
+    refresh_meta();
   }
 
   virtual std::unique_ptr<Object> clone() override {
@@ -189,7 +165,7 @@ class SFSObject : public Object {
    * Obtain a Read Operation.
    */
   virtual std::unique_ptr<ReadOp> get_read_op() override {
-    return std::make_unique<SFSObject::SFSReadOp>(this, nullptr);
+    return std::make_unique<SFSObject::SFSReadOp>(this);
   }
   /**
    * Obtain a Delete Operation.
@@ -226,15 +202,16 @@ class SFSObject : public Object {
     return 0;
   }
 
+  sfs::ObjectRef get_object_ref() {    
+    return objref;
+  }
+
   std::filesystem::path get_data_path();
   std::filesystem::path get_metadata_path();
 
+  void refresh_meta();
+
   const std::string get_cls_name() { return "object"; }
-  void write_meta();
-  void load_meta();
-  void refresh_meta() {
-    load_meta();
-  }
 };
 
 } // ns rgw::sal

src/rgw/store/sfs/types.h
@@ -1,0 +1,171 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * SFS SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SFS_TYPES_H
+#define RGW_STORE_SFS_TYPES_H
+
+#include <exception>
+#include <string>
+#include <memory>
+#include <map>
+#include <set>
+
+#include "common/ceph_mutex.h"
+
+#include "rgw_sal.h"
+#include "rgw/store/sfs/uuid_path.h"
+
+namespace rgw::sal::sfs {
+
+
+struct UnknownObjectException : public std::exception { };
+
+
+struct Object {
+
+  struct Meta {
+    size_t size;
+    std::string etag;
+    ceph::real_time mtime;
+    ceph::real_time set_mtime;
+    ceph::real_time delete_at;
+    std::map<std::string, bufferlist> attrs;
+  };
+
+  std::string name;
+  UUIDPath path;
+  Meta meta;
+  bool deleted;
+
+  Object(const std::string &_name) 
+  : name(_name), path(UUIDPath::create()), deleted(false) { }
+
+};
+
+using ObjectRef = std::shared_ptr<Object>;
+
+class Bucket {
+
+  SFStore *store;
+  const std::string name;
+  rgw_bucket bucket;
+  RGWUserInfo owner;
+  ceph::real_time creation_time;
+
+
+ public:
+  std::map<std::string, ObjectRef> objects;
+  ceph::mutex obj_map_lock = ceph::make_mutex("obj_map_lock");
+  std::map<std::string, ObjectRef> creating;
+  std::set<ObjectRef> deleted;
+
+  Bucket(const Bucket&) = default;
+
+ public:
+  Bucket(
+    SFStore *_store, const rgw_bucket &_bucket, const RGWUserInfo &_owner
+  ) : store(_store), name(_bucket.name), bucket(_bucket), owner(_owner) {
+    creation_time = ceph::real_clock::now();
+  }
+
+  const std::string get_name() const {
+    return name;
+  }
+
+  rgw_bucket& get_bucket() {
+    return bucket;
+  }
+
+  RGWUserInfo& get_owner() {
+    return owner;
+  }
+
+  ceph::real_time get_creation_time() {
+    return creation_time;
+  }
+
+  ObjectRef get_or_create(const std::string &name) {
+    std::lock_guard l(obj_map_lock);
+
+    {
+      auto it = objects.find(name);
+      if (it != objects.end()) {
+        return it->second;
+      }
+    }
+
+    // is object already being created?
+    //   this will potentially clash between two competing tasks.
+    //   deal with that later.
+    {
+      auto it = creating.find(name);
+      if (it != creating.end()) {
+        return it->second;
+      }
+    }
+
+    // create new object
+    ObjectRef obj = std::make_shared<Object>(name);
+    creating[name] = obj;
+    return obj;
+  }
+
+  ObjectRef get(const std::string &name) {
+    auto it = objects.find(name);
+    if (it == objects.end()) {
+      throw UnknownObjectException();
+    }
+    return objects[name];
+  }
+
+  void finish(const std::string &name) {
+    std::lock_guard l(obj_map_lock);
+
+    auto it = creating.find(name);
+    if (it == creating.end()) {
+      return;
+    }
+
+    // finished creating the object
+    ceph_assert(objects.count(name) == 0);
+    objects[name] = creating[name];
+    creating.erase(name);
+  }
+
+  void delete_object(ObjectRef objref) {
+    std::lock_guard l(obj_map_lock);
+
+    objref->deleted = true;
+
+    auto it = objects.find(objref->name);
+    if (it == objects.end()) {
+      // nothing to do.
+      return;
+    }
+
+    ObjectRef found = it->second;
+    if (!found->path.match(objref->path)) {
+      // different objects, nothing to do.
+      return;
+    }
+
+    deleted.insert(objref);
+    objects.erase(it);
+  }
+};
+
+using BucketRef = std::shared_ptr<Bucket>;
+
+}  // ns rgw::sal::sfs
+
+#endif // RGW_STORE_SFS_TYPES_H

src/rgw/store/sfs/writer.h
@@ -16,7 +16,7 @@
 
 #include <memory>
 #include "rgw_sal.h"
-#include "store/sfs/bucket_mgr.h"
+#include "store/sfs/bucket.h"
 
 namespace rgw::sal {
 
@@ -27,7 +27,8 @@ class SFSAtomicWriter : public Writer {
  protected:
   rgw::sal::SFStore *store;
   SFSObject obj;
-  BucketMgrRef mgr;
+  sfs::BucketRef bucketref;
+  sfs::ObjectRef objref;
   const rgw_user &owner;
   const rgw_placement_rule *placement_rule;
   uint64_t olh_epoch;
@@ -40,7 +41,7 @@ class SFSAtomicWriter : public Writer {
     optional_yield _y,
     std::unique_ptr<rgw::sal::Object> _head_obj,
     SFStore *_store,
-    BucketMgrRef _mgr,
+    sfs::BucketRef bucketref,
     const rgw_user& _owner,
     const rgw_placement_rule *_ptail_placement_rule,
     uint64_t _olh_epoch,

irq0/ceph

src/rgw/driver/sfs/object.h
@@ -14,7 +14,13 @@
 #ifndef RGW_STORE_SFS_OBJECT_H
 #define RGW_STORE_SFS_OBJECT_H
 
+#include <filesystem>
+
 #include "rgw_sal.h"
+#include "uuid_path.h"
+#include "rgw/driver/sfs/bucket.h"
+#include "rgw/driver/sfs/types.h"
+
 
 namespace rgw::sal {
 
@@ -24,44 +30,12 @@ class SFSObject : public StoreObject {
  private:
   SFStore *store;
   RGWAccessControlPolicy acls;
+  sfs::BucketRef bucketref;
+  sfs::ObjectRef objref;
  protected:
   SFSObject(SFSObject&) = default;
-  void init() {
-    load_meta();
-  }
 
  public:
-
-  struct Meta {
-    size_t size;
-    std::string etag;
-    ceph::real_time mtime;
-    ceph::real_time set_mtime;
-    ceph::real_time delete_at;
-    std::map<std::string, bufferlist> attrs;
-
-    Meta() { }
-    ~Meta() = default;
-
-    void dump(Formatter *f) const {
-      encode_json("size", size, f);
-      encode_json("etag", etag, f);
-      encode_json("mtime", mtime, f);
-      encode_json("set_mtime", set_mtime, f);
-      encode_json("delete_at", delete_at, f);
-      encode_json("attrs", attrs, f);
-    }
-
-    void decode_json(JSONObj *obj) {
-      JSONDecoder::decode_json("size", size, obj);
-      JSONDecoder::decode_json("etag", etag, obj);
-      JSONDecoder::decode_json("mtime", mtime, obj);
-      JSONDecoder::decode_json("set_mtime", set_mtime, obj);
-      JSONDecoder::decode_json("delete_at", delete_at, obj);
-      JSONDecoder::decode_json("attrs", attrs, obj);
-    }
-  };
-  SFSObject::Meta meta;
 
   /**
    * reads an object's contents.
@@ -69,10 +43,11 @@ class SFSObject : public StoreObject {
   struct SFSReadOp : public ReadOp {
    private:
     SFSObject *source;
-    RGWObjectCtx *rctx;
+    sfs::ObjectRef objref;
+    std::filesystem::path objdata;
 
    public:
-    SFSReadOp(SFSObject *_source, RGWObjectCtx *_rctx);
+    SFSReadOp(SFSObject *_source);
 
     virtual int prepare(optional_yield y,
                         const DoutPrefixProvider *dpp) override;
@@ -92,10 +67,10 @@ class SFSObject : public StoreObject {
   struct SFSDeleteOp : public DeleteOp {
    private:
     SFSObject *source;
-    BucketMgrRef mgr;
+    sfs::BucketRef bucketref;
 
    public:
-    SFSDeleteOp(SFSObject *_source, BucketMgrRef _mgr);
+    SFSDeleteOp(SFSObject *_source, sfs::BucketRef _bucketref);
     virtual int delete_obj(const DoutPrefixProvider *dpp,
                            optional_yield y) override;
 
@@ -112,9 +87,10 @@ class SFSObject : public StoreObject {
   SFSObject(
     SFStore *_st,
     const rgw_obj_key &_k,
-    Bucket *_b
-  ) : StoreObject(_k, _b), store(_st) {
-    init();
+    Bucket *_b,
+    sfs::BucketRef _bucket
+  ) : StoreObject(_k, _b), store(_st), bucketref(_bucket) {
+    refresh_meta();
   }
 
   virtual std::unique_ptr<Object> clone() override {
@@ -187,7 +163,7 @@ class SFSObject : public StoreObject {
    * Obtain a Read Operation.
    */
   virtual std::unique_ptr<ReadOp> get_read_op() override {
-    return std::make_unique<SFSObject::SFSReadOp>(this, nullptr);
+    return std::make_unique<SFSObject::SFSReadOp>(this);
   }
   /**
    * Obtain a Delete Operation.
@@ -219,15 +195,16 @@ class SFSObject : public StoreObject {
     return 0;
   }
 
+  sfs::ObjectRef get_object_ref() {    
+    return objref;
+  }
+
   std::filesystem::path get_data_path();
   std::filesystem::path get_metadata_path();
 
+  void refresh_meta();
+
   const std::string get_cls_name() { return "object"; }
-  void write_meta();
-  void load_meta();
-  void refresh_meta() {
-    load_meta();
-  }
 };
 
 } // ns rgw::sal

src/rgw/driver/sfs/types.h
@@ -1,0 +1,171 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * SFS SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SFS_TYPES_H
+#define RGW_STORE_SFS_TYPES_H
+
+#include <exception>
+#include <string>
+#include <memory>
+#include <map>
+#include <set>
+
+#include "common/ceph_mutex.h"
+
+#include "rgw_sal.h"
+#include "rgw/driver/sfs/uuid_path.h"
+
+namespace rgw::sal::sfs {
+
+
+struct UnknownObjectException : public std::exception { };
+
+
+struct Object {
+
+  struct Meta {
+    size_t size;
+    std::string etag;
+    ceph::real_time mtime;
+    ceph::real_time set_mtime;
+    ceph::real_time delete_at;
+    std::map<std::string, bufferlist> attrs;
+  };
+
+  std::string name;
+  UUIDPath path;
+  Meta meta;
+  bool deleted;
+
+  Object(const std::string &_name) 
+  : name(_name), path(UUIDPath::create()), deleted(false) { }
+
+};
+
+using ObjectRef = std::shared_ptr<Object>;
+
+class Bucket {
+
+  SFStore *store;
+  const std::string name;
+  rgw_bucket bucket;
+  RGWUserInfo owner;
+  ceph::real_time creation_time;
+
+
+ public:
+  std::map<std::string, ObjectRef> objects;
+  ceph::mutex obj_map_lock = ceph::make_mutex("obj_map_lock");
+  std::map<std::string, ObjectRef> creating;
+  std::set<ObjectRef> deleted;
+
+  Bucket(const Bucket&) = default;
+
+ public:
+  Bucket(
+    SFStore *_store, const rgw_bucket &_bucket, const RGWUserInfo &_owner
+  ) : store(_store), name(_bucket.name), bucket(_bucket), owner(_owner) {
+    creation_time = ceph::real_clock::now();
+  }
+
+  const std::string get_name() const {
+    return name;
+  }
+
+  rgw_bucket& get_bucket() {
+    return bucket;
+  }
+
+  RGWUserInfo& get_owner() {
+    return owner;
+  }
+
+  ceph::real_time get_creation_time() {
+    return creation_time;
+  }
+
+  ObjectRef get_or_create(const std::string &name) {
+    std::lock_guard l(obj_map_lock);
+
+    {
+      auto it = objects.find(name);
+      if (it != objects.end()) {
+        return it->second;
+      }
+    }
+
+    // is object already being created?
+    //   this will potentially clash between two competing tasks.
+    //   deal with that later.
+    {
+      auto it = creating.find(name);
+      if (it != creating.end()) {
+        return it->second;
+      }
+    }
+
+    // create new object
+    ObjectRef obj = std::make_shared<Object>(name);
+    creating[name] = obj;
+    return obj;
+  }
+
+  ObjectRef get(const std::string &name) {
+    auto it = objects.find(name);
+    if (it == objects.end()) {
+      throw UnknownObjectException();
+    }
+    return objects[name];
+  }
+
+  void finish(const std::string &name) {
+    std::lock_guard l(obj_map_lock);
+
+    auto it = creating.find(name);
+    if (it == creating.end()) {
+      return;
+    }
+
+    // finished creating the object
+    ceph_assert(objects.count(name) == 0);
+    objects[name] = creating[name];
+    creating.erase(name);
+  }
+
+  void delete_object(ObjectRef objref) {
+    std::lock_guard l(obj_map_lock);
+
+    objref->deleted = true;
+
+    auto it = objects.find(objref->name);
+    if (it == objects.end()) {
+      // nothing to do.
+      return;
+    }
+
+    ObjectRef found = it->second;
+    if (!found->path.match(objref->path)) {
+      // different objects, nothing to do.
+      return;
+    }
+
+    deleted.insert(objref);
+    objects.erase(it);
+  }
+};
+
+using BucketRef = std::shared_ptr<Bucket>;
+
+}  // ns rgw::sal::sfs
+
+#endif // RGW_STORE_SFS_TYPES_H

src/rgw/driver/sfs/writer.h
@@ -17,7 +17,7 @@
 #include <memory>
 #include "rgw_sal.h"
 #include "rgw_sal_store.h"
-#include "driver/sfs/bucket_mgr.h"
+#include "driver/sfs/bucket.h"
 
 namespace rgw::sal {
 
@@ -28,7 +28,8 @@ class SFSAtomicWriter : public StoreWriter {
  protected:
   rgw::sal::SFStore *store;
   SFSObject obj;
-  BucketMgrRef mgr;
+  sfs::BucketRef bucketref;
+  sfs::ObjectRef objref;
   const rgw_user &owner;
   const rgw_placement_rule *placement_rule;
   uint64_t olh_epoch;
@@ -41,7 +42,7 @@ class SFSAtomicWriter : public StoreWriter {
     optional_yield _y,
     rgw::sal::Object* _head_obj,
     SFStore *_store,
-    BucketMgrRef _mgr,
+    sfs::BucketRef bucketref,
     const rgw_user& _owner,
     const rgw_placement_rule *_ptail_placement_rule,
     uint64_t _olh_epoch,