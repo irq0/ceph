wipwd/s3gw-ceph

rgw/sfs: Add object versioning.
Adds object versioning using the sqlite versioned objects table.

This version keeps the last available (updated) version in memory for
faster access and queries sqlite metadata for specific versions.

Also implements enabling bucket versioning.

Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>

Fixes: https://github.com/aquarist-labs/s3gw/issues/9

src/rgw/store/sfs/bucket.cc
@@ -14,6 +14,7 @@
 #include <fstream>
 #include "rgw_sal_sfs.h"
 #include "store/sfs/multipart.h"
+#include "store/sfs/sqlite/sqlite_versioned_objects.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -54,9 +55,13 @@ std::unique_ptr<Object> SFSBucket::get_object(const rgw_obj_key &key) {
 /**
  * List objects in this bucket.
  */
-int SFSBucket::list(const DoutPrefixProvider *dpp, ListParams &, int,
+int SFSBucket::list(const DoutPrefixProvider *dpp, ListParams &params, int max,
                            ListResults &results, optional_yield y) {
   lsfs_dout(dpp, 10) << "iterate bucket " << get_name() << dendl;
+
+  if (params.list_versions) {
+    return list_versions(dpp, params, max, results, y);
+  }
   
   std::lock_guard l(bucket->obj_map_lock);
   for (const auto &[name, objref]: bucket->objects) {
@@ -64,12 +69,43 @@ int SFSBucket::list(const DoutPrefixProvider *dpp, ListParams &, int,
 
     auto obj = _get_object(objref);
     rgw_bucket_dir_entry dirent;
-    dirent.key = cls_rgw_obj_key(name);
+    dirent.key = cls_rgw_obj_key(name, objref->instance);
     dirent.meta.accounted_size = obj->get_obj_size();
     dirent.meta.mtime = obj->get_mtime();
+    dirent.meta.etag = objref->meta.etag;
+    dirent.meta.owner_display_name = bucket->get_owner().display_name;
+    dirent.meta.owner = bucket->get_owner().user_id.id;
     results.objs.push_back(dirent);
   }
   
+  lsfs_dout(dpp, 10) << "found " << results.objs.size() << " objects" << dendl;
+  return 0;
+}
+
+int SFSBucket::list_versions(const DoutPrefixProvider *dpp, ListParams &params,
+                      int, ListResults &results, optional_yield y) {
+  std::lock_guard l(bucket->obj_map_lock);
+  for (const auto &[name, objref]: bucket->objects) {
+    lsfs_dout(dpp, 10) << "object: " << name << dendl;
+    // get all available versions from db
+    sfs::sqlite::SQLiteVersionedObjects db_versioned_objects(store->db_conn);
+    auto last_version = db_versioned_objects.get_last_versioned_object(objref->path.get_uuid());
+    auto object_versions = db_versioned_objects.get_versioned_objects(objref->path.get_uuid());
+    for (const auto &object_version: object_versions) {
+      rgw_bucket_dir_entry dirent;
+      dirent.key = cls_rgw_obj_key(objref->name, object_version.version_id);
+      dirent.meta.accounted_size = object_version.size;
+      dirent.meta.mtime = object_version.creation_time;
+      dirent.meta.etag = objref->meta.etag;
+      dirent.flags = rgw_bucket_dir_entry::FLAG_VER;
+      if (last_version.has_value() && last_version->id == object_version.id) {
+        dirent.flags |= rgw_bucket_dir_entry::FLAG_CURRENT;
+      }
+      dirent.meta.owner_display_name = bucket->get_owner().display_name;
+      dirent.meta.owner = bucket->get_owner().user_id.id;
+      results.objs.push_back(dirent);
+    }
+  }
   lsfs_dout(dpp, 10) << "found " << results.objs.size() << " objects" << dendl;
   return 0;
 }
@@ -239,8 +275,13 @@ int SFSBucket::check_bucket_shards(const DoutPrefixProvider *dpp) {
 }
 int SFSBucket::put_info(const DoutPrefixProvider *dpp, bool exclusive,
                                ceph::real_time mtime) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+  sfs::sqlite::DBOPBucketInfo store_info;
+  store_info.binfo = info;
+
+  auto meta_buckets = sfs::get_meta_buckets(store->db_conn);
+  meta_buckets->store_bucket(store_info);
+  store->_refresh_buckets_safe();
+  return 0;
 }
 
 } // ns rgw::sal

src/rgw/store/sfs/object.cc
@@ -13,6 +13,7 @@
  */
 #include "rgw_sal_sfs.h"
 #include "store/sfs/object.h"
+#include "store/sfs/sqlite/sqlite_versioned_objects.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -30,13 +31,13 @@ int SFSObject::SFSReadOp::prepare(
 
   source->refresh_meta();
   objref = source->get_object_ref();
-  if (!objref) {
+  if (!objref || objref->deleted) {
     // at this point, we don't have an objectref because
     // the object does not exist.
     return -ENOENT;
   }
 
-  objdata = source->store->get_data_path() / objref->path.to_path();
+  objdata = source->store->get_data_path() / objref->get_storage_path();
   if (!std::filesystem::exists(objdata)) {
     lsfs_dout(dpp, 10) << "object data not found at " << objdata << dendl;
     return -ENOENT;
@@ -77,8 +78,6 @@ int SFSObject::SFSReadOp::read(int64_t ofs, int64_t end,
                      << ", end: " << end
                      << ", len: " << len << dendl;
 
-  // auto objpath = source->get_data_path();
-  // ceph_assert(std::filesystem::exists(objpath));
   ceph_assert(std::filesystem::exists(objdata));
 
   std::string error;
@@ -133,7 +132,9 @@ int SFSObject::SFSDeleteOp::delete_obj(
     optional_yield y
 ) {
   lsfs_dout(dpp, 10) << "bucket: " << source->bucket->get_name()
-                     << ", object: " << source->get_name() << dendl;
+                     << ", object: " << source->get_name()
+                     << ", instance: " << source->get_instance()
+                     << dendl;
 
   // do the quick and dirty thing for now
   ceph_assert(bucketref);
@@ -207,11 +208,11 @@ int SFSObject::copy_object(
   ceph_assert(dst_bucket_ref);
 
   std::filesystem::path srcpath =
-    store->get_data_path() / objref->path.to_path();
+    store->get_data_path() / objref->get_storage_path();
 
-  sfs::ObjectRef dstref = dst_bucket_ref->get_or_create(dst_object->get_name());
+  sfs::ObjectRef dstref = dst_bucket_ref->get_or_create(dst_object->get_key());
   std::filesystem::path dstpath =
-    store->get_data_path() / dstref->path.to_path();
+    store->get_data_path() / dstref->get_storage_path();
 
   if (std::filesystem::exists(dstpath)) {
     // this breaks S3 semantics: as far as we understand, a copy to an existing
@@ -222,6 +223,7 @@ int SFSObject::copy_object(
     return -EEXIST;
   }
 
+  dstref->metadata_change_version_state(store, ObjectState::WRITING);
   lsfs_dout(dpp, 10) << "copying file from '" << srcpath << "' to '"
                      << dstpath << "'" << dendl;
   std::filesystem::create_directories(dstpath.parent_path());
@@ -234,15 +236,18 @@ int SFSObject::copy_object(
 
   dstref->meta = objref->meta;
   dstref->meta.mtime = ceph::real_clock::now();
-  dst_bucket_ref->finish(dpp, dst_object->get_name());
+  dstref->metadata_finish(store);
 
   return 0;
 }
 
-/** TODO Create a randomized instance ID for this object */
 void SFSObject::gen_rand_obj_instance_name() {
-  ldout(store->ceph_context(), 10) << __func__ << ": TODO" << dendl;
-  return;
+#define OBJ_INSTANCE_LEN 32
+  char buf[OBJ_INSTANCE_LEN + 1];
+
+  gen_rand_alphanumeric_no_underscore(store->ceph_context(), buf, OBJ_INSTANCE_LEN);
+
+  state.obj.key.set_instance(buf);
 }
 
 int SFSObject::get_obj_attrs(optional_yield y,
@@ -359,14 +364,26 @@ void SFSObject::refresh_meta() {
     bucketref = store->get_bucket_ref(bucket->get_name());
   }
   try {
-    objref = bucketref->get(get_name());;
+    objref = bucketref->get(get_name());
   } catch (sfs::UnknownObjectException &e) {
     // object probably not created yet?
     return;
   }
-  set_obj_size(objref->meta.size);
-  set_attrs(objref->meta.attrs);
-  state.mtime = objref->meta.mtime;
+  auto meta = objref->meta;
+  if (!get_instance().empty() && get_instance() != objref->instance) {
+    // object specific version requested and it's not the last one
+    sfs::sqlite::SQLiteVersionedObjects db_versioned_objects(store->db_conn);
+    auto db_version = db_versioned_objects.get_versioned_object(get_instance());
+    auto uuid = objref->path.get_uuid();
+    auto deleted = db_version->object_state == ObjectState::DELETED;
+    objref = std::make_shared<sfs::Object>(get_name(), uuid, deleted);
+    objref->version_id = db_version->id;
+    set_obj_size(db_version->size);
+  } else {
+    set_obj_size(meta.size);
+  }
+  set_attrs(meta.attrs);
+  state.mtime = meta.mtime;
 }
 
 } // ns rgw::sal

src/rgw/store/sfs/types.cc
@@ -19,22 +19,99 @@
 #include "rgw/store/sfs/types.h"
 #include "rgw/store/sfs/sqlite/sqlite_buckets.h"
 #include "rgw/store/sfs/sqlite/sqlite_objects.h"
+#include "rgw/store/sfs/sqlite/sqlite_versioned_objects.h"
+#include "rgw/store/sfs/object_state.h"
 
 namespace rgw::sal::sfs {
 
+std::filesystem::path Object::get_storage_path() const {
+  return path.to_path() / std::to_string(version_id);
+}
+
+void Object::metadata_init(SFStore *store, const std::string & bucket_name,
+                           bool new_object, bool new_version) {
+  sqlite::DBOPObjectInfo oinfo;
+  oinfo.uuid = path.get_uuid();
+  oinfo.bucket_name = bucket_name;
+  oinfo.name = name;
+
+  if (new_object) {
+    sqlite::SQLiteObjects dbobjs(store->db_conn);
+    dbobjs.store_object(oinfo);
+  }
+
+  if (new_version) {
+    sqlite::DBOPVersionedObjectInfo version_info;
+    version_info.object_id = path.get_uuid();
+    version_info.object_state = ObjectState::OPEN;
+    version_info.version_id = instance;
+    sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+    version_id = db_versioned_objs.insert_versioned_object(version_info);
+  }
+}
+
+void Object::metadata_change_version_state(SFStore *store, ObjectState state) {
+  sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+  auto versioned_object = db_versioned_objs.get_versioned_object(version_id);
+  ceph_assert(versioned_object.has_value());
+  versioned_object->object_state = state;
+  if (state == ObjectState::DELETED) {
+    deleted = true;
+    versioned_object->deletion_time = ceph::real_clock::now();
+  }
+  db_versioned_objs.store_versioned_object(*versioned_object);
+}
+
+void Object::metadata_finish(SFStore *store) {
+  sqlite::SQLiteObjects dbobjs(store->db_conn);
+  auto db_object = dbobjs.get_object(path.get_uuid());
+  ceph_assert(db_object.has_value());
+  db_object->size = meta.size;
+  db_object->etag = meta.etag;
+  db_object->mtime = meta.mtime;
+  db_object->set_mtime = meta.set_mtime;
+  db_object->delete_at = meta.delete_at;
+  db_object->attrs = meta.attrs;
+  dbobjs.store_object(*db_object);
+
+  sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+  auto db_versioned_object = db_versioned_objs.get_versioned_object(version_id);
+  ceph_assert(db_versioned_object.has_value());
+  // TODO calculate checksum. Is it already calculated while writing?
+  db_versioned_object->size = meta.size;
+  db_versioned_object->creation_time = meta.mtime;
+  db_versioned_object->object_state = ObjectState::COMMITTED;
+  db_versioned_objs.store_versioned_object(*db_versioned_object);
+}
+
+ObjectRef Bucket::get_or_create(const rgw_obj_key &key) {
+  std::lock_guard l(obj_map_lock);
+  ObjectRef obj = nullptr;
+  auto new_object = true;
+  auto create_new_version = true;
+  auto it = objects.find(key.name);
+  if (it != objects.end()) {
+    obj = it->second;
+    new_object = false;
+    if (key.instance.empty() || key.instance == obj->instance) {
+      create_new_version = false;
+    } else {
+      obj->instance = key.instance;
+    }
+  } else {
+    obj = std::make_shared<Object>(key);
+    objects[key.name] = obj;
+  }
+  obj->metadata_init(store, name, new_object, create_new_version);
+
+  return obj;
+}
 
 void Bucket::finish(const DoutPrefixProvider *dpp, const std::string &objname) {
   std::lock_guard l(obj_map_lock);
 
-  auto it = creating.find(objname);
-  if (it == creating.end()) {
-    return;
-  }
 
   // finished creating the object
-  ceph_assert(objects.count(objname) == 0);
-  objects[objname] = creating[objname];
-  creating.erase(objname);
 
   ObjectRef ref = objects[objname];
   sqlite::DBOPObjectInfo oinfo;
@@ -52,10 +129,38 @@ void Bucket::finish(const DoutPrefixProvider *dpp, const std::string &objname) {
   dbobjs.store_object(oinfo);
 }
 
+void Bucket::delete_object(ObjectRef objref) {
+  std::lock_guard l(obj_map_lock);
+
+  sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+  // get the last available version to make a copy changing the object state to DELETED
+  auto last_version = db_versioned_objs.get_last_versioned_object(objref->path.get_uuid());
+  ceph_assert(last_version.has_value());
+  last_version->object_state = ObjectState::DELETED;
+  last_version->deletion_time = ceph::real_clock::now();
+
+  if (last_version->version_id != "") {
+    // generate a new version id
+    #define OBJ_INSTANCE_LEN 32
+    char buf[OBJ_INSTANCE_LEN + 1];
+    gen_rand_alphanumeric_no_underscore(store->ceph_context(), buf, OBJ_INSTANCE_LEN);
+    last_version->version_id = std::string(buf);
+    objref->instance = last_version->version_id;
+    // insert a new deleted version
+    db_versioned_objs.insert_versioned_object(*last_version);
+  } else {
+    db_versioned_objs.store_versioned_object(*last_version);
+  }
+  objref->deleted = true;
+}
+
 void Bucket::_refresh_objects() {
   sqlite::SQLiteObjects objs(store->db_conn);
   auto existing = objs.get_objects(name);
   for (const auto &obj : existing) {
+    sqlite::SQLiteVersionedObjects objs_versions(store->db_conn);
+    auto last_version = objs_versions.get_last_versioned_object(obj.uuid);
+    ceph_assert(last_version.has_value());
     ObjectRef ref = std::make_shared<Object>(obj.name, obj.uuid, false);
     ref->meta.size = obj.size;
     ref->meta.etag = obj.etag;
@@ -63,6 +168,10 @@ void Bucket::_refresh_objects() {
     ref->meta.set_mtime = obj.set_mtime;
     ref->meta.delete_at = obj.delete_at;
     ref->meta.attrs = obj.attrs;
+    ref->version_id = last_version->id;
+    ref->instance = last_version->version_id;
+    ref->deleted = (last_version->object_state == ObjectState::DELETED);
+
     objects[obj.name] = ref;
   }
 }

irq0/ceph

src/rgw/driver/sfs/bucket.cc
@@ -14,6 +14,7 @@
 #include <fstream>
 #include "rgw_sal_sfs.h"
 #include "driver/sfs/multipart.h"
+#include "driver/sfs/sqlite/sqlite_versioned_objects.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -54,9 +55,13 @@ std::unique_ptr<Object> SFSBucket::get_object(const rgw_obj_key &key) {
 /**
  * List objects in this bucket.
  */
-int SFSBucket::list(const DoutPrefixProvider *dpp, ListParams &, int,
+int SFSBucket::list(const DoutPrefixProvider *dpp, ListParams &params, int max,
                            ListResults &results, optional_yield y) {
   lsfs_dout(dpp, 10) << "iterate bucket " << get_name() << dendl;
+
+  if (params.list_versions) {
+    return list_versions(dpp, params, max, results, y);
+  }
   
   std::lock_guard l(bucket->obj_map_lock);
   for (const auto &[name, objref]: bucket->objects) {
@@ -64,12 +69,43 @@ int SFSBucket::list(const DoutPrefixProvider *dpp, ListParams &, int,
 
     auto obj = _get_object(objref);
     rgw_bucket_dir_entry dirent;
-    dirent.key = cls_rgw_obj_key(name);
+    dirent.key = cls_rgw_obj_key(name, objref->instance);
     dirent.meta.accounted_size = obj->get_obj_size();
     dirent.meta.mtime = obj->get_mtime();
+    dirent.meta.etag = objref->meta.etag;
+    dirent.meta.owner_display_name = bucket->get_owner().display_name;
+    dirent.meta.owner = bucket->get_owner().user_id.id;
     results.objs.push_back(dirent);
   }
   
+  lsfs_dout(dpp, 10) << "found " << results.objs.size() << " objects" << dendl;
+  return 0;
+}
+
+int SFSBucket::list_versions(const DoutPrefixProvider *dpp, ListParams &params,
+                      int, ListResults &results, optional_yield y) {
+  std::lock_guard l(bucket->obj_map_lock);
+  for (const auto &[name, objref]: bucket->objects) {
+    lsfs_dout(dpp, 10) << "object: " << name << dendl;
+    // get all available versions from db
+    sfs::sqlite::SQLiteVersionedObjects db_versioned_objects(store->db_conn);
+    auto last_version = db_versioned_objects.get_last_versioned_object(objref->path.get_uuid());
+    auto object_versions = db_versioned_objects.get_versioned_objects(objref->path.get_uuid());
+    for (const auto &object_version: object_versions) {
+      rgw_bucket_dir_entry dirent;
+      dirent.key = cls_rgw_obj_key(objref->name, object_version.version_id);
+      dirent.meta.accounted_size = object_version.size;
+      dirent.meta.mtime = object_version.creation_time;
+      dirent.meta.etag = objref->meta.etag;
+      dirent.flags = rgw_bucket_dir_entry::FLAG_VER;
+      if (last_version.has_value() && last_version->id == object_version.id) {
+        dirent.flags |= rgw_bucket_dir_entry::FLAG_CURRENT;
+      }
+      dirent.meta.owner_display_name = bucket->get_owner().display_name;
+      dirent.meta.owner = bucket->get_owner().user_id.id;
+      results.objs.push_back(dirent);
+    }
+  }
   lsfs_dout(dpp, 10) << "found " << results.objs.size() << " objects" << dendl;
   return 0;
 }
@@ -238,8 +274,13 @@ int SFSBucket::check_bucket_shards(const DoutPrefixProvider *dpp) {
 }
 int SFSBucket::put_info(const DoutPrefixProvider *dpp, bool exclusive,
                                ceph::real_time mtime) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+  sfs::sqlite::DBOPBucketInfo store_info;
+  store_info.binfo = info;
+
+  auto meta_buckets = sfs::get_meta_buckets(store->db_conn);
+  meta_buckets->store_bucket(store_info);
+  store->_refresh_buckets_safe();
+  return 0;
 }
 
 } // ns rgw::sal

src/rgw/driver/sfs/object.cc
@@ -13,6 +13,7 @@
  */
 #include "rgw_sal_sfs.h"
 #include "driver/sfs/object.h"
+#include "driver/sfs/sqlite/sqlite_versioned_objects.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -30,13 +31,13 @@ int SFSObject::SFSReadOp::prepare(
 
   source->refresh_meta();
   objref = source->get_object_ref();
-  if (!objref) {
+  if (!objref || objref->deleted) {
     // at this point, we don't have an objectref because
     // the object does not exist.
     return -ENOENT;
   }
 
-  objdata = source->store->get_data_path() / objref->path.to_path();
+  objdata = source->store->get_data_path() / objref->get_storage_path();
   if (!std::filesystem::exists(objdata)) {
     lsfs_dout(dpp, 10) << "object data not found at " << objdata << dendl;
     return -ENOENT;
@@ -77,8 +78,6 @@ int SFSObject::SFSReadOp::read(int64_t ofs, int64_t end,
                      << ", end: " << end
                      << ", len: " << len << dendl;
 
-  // auto objpath = source->get_data_path();
-  // ceph_assert(std::filesystem::exists(objpath));
   ceph_assert(std::filesystem::exists(objdata));
 
   std::string error;
@@ -133,7 +132,9 @@ int SFSObject::SFSDeleteOp::delete_obj(
     optional_yield y
 ) {
   lsfs_dout(dpp, 10) << "bucket: " << source->bucket->get_name()
-                     << ", object: " << source->get_name() << dendl;
+                     << ", object: " << source->get_name()
+                     << ", instance: " << source->get_instance()
+                     << dendl;
 
   // do the quick and dirty thing for now
   ceph_assert(bucketref);
@@ -199,11 +200,11 @@ int SFSObject::copy_object(
   ceph_assert(dst_bucket_ref);
 
   std::filesystem::path srcpath =
-    store->get_data_path() / objref->path.to_path();
+    store->get_data_path() / objref->get_storage_path();
 
-  sfs::ObjectRef dstref = dst_bucket_ref->get_or_create(dst_object->get_name());
+  sfs::ObjectRef dstref = dst_bucket_ref->get_or_create(dst_object->get_key());
   std::filesystem::path dstpath =
-    store->get_data_path() / dstref->path.to_path();
+    store->get_data_path() / dstref->get_storage_path();
 
   if (std::filesystem::exists(dstpath)) {
     // this breaks S3 semantics: as far as we understand, a copy to an existing
@@ -214,6 +215,7 @@ int SFSObject::copy_object(
     return -EEXIST;
   }
 
+  dstref->metadata_change_version_state(store, ObjectState::WRITING);
   lsfs_dout(dpp, 10) << "copying file from '" << srcpath << "' to '"
                      << dstpath << "'" << dendl;
   std::filesystem::create_directories(dstpath.parent_path());
@@ -226,15 +228,18 @@ int SFSObject::copy_object(
 
   dstref->meta = objref->meta;
   dstref->meta.mtime = ceph::real_clock::now();
-  dst_bucket_ref->finish(dpp, dst_object->get_name());
+  dstref->metadata_finish(store);
 
   return 0;
 }
 
-/** TODO Create a randomized instance ID for this object */
 void SFSObject::gen_rand_obj_instance_name() {
-  ldout(store->ceph_context(), 10) << __func__ << ": TODO" << dendl;
-  return;
+#define OBJ_INSTANCE_LEN 32
+  char buf[OBJ_INSTANCE_LEN + 1];
+
+  gen_rand_alphanumeric_no_underscore(store->ceph_context(), buf, OBJ_INSTANCE_LEN);
+
+  state.obj.key.set_instance(buf);
 }
 
 int SFSObject::get_obj_attrs(optional_yield y,
@@ -344,14 +349,26 @@ void SFSObject::refresh_meta() {
     bucketref = store->get_bucket_ref(bucket->get_name());
   }
   try {
-    objref = bucketref->get(get_name());;
+    objref = bucketref->get(get_name());
   } catch (sfs::UnknownObjectException &e) {
     // object probably not created yet?
     return;
   }
-  set_obj_size(objref->meta.size);
-  set_attrs(objref->meta.attrs);
-  state.mtime = objref->meta.mtime;
+  auto meta = objref->meta;
+  if (!get_instance().empty() && get_instance() != objref->instance) {
+    // object specific version requested and it's not the last one
+    sfs::sqlite::SQLiteVersionedObjects db_versioned_objects(store->db_conn);
+    auto db_version = db_versioned_objects.get_versioned_object(get_instance());
+    auto uuid = objref->path.get_uuid();
+    auto deleted = db_version->object_state == ObjectState::DELETED;
+    objref = std::make_shared<sfs::Object>(get_name(), uuid, deleted);
+    objref->version_id = db_version->id;
+    set_obj_size(db_version->size);
+  } else {
+    set_obj_size(meta.size);
+  }
+  set_attrs(meta.attrs);
+  state.mtime = meta.mtime;
 }
 
 } // ns rgw::sal

src/rgw/driver/sfs/types.cc
@@ -19,22 +19,99 @@
 #include "rgw/driver/sfs/types.h"
 #include "rgw/driver/sfs/sqlite/sqlite_buckets.h"
 #include "rgw/driver/sfs/sqlite/sqlite_objects.h"
+#include "rgw/driver/sfs/sqlite/sqlite_versioned_objects.h"
+#include "rgw/driver/sfs/object_state.h"
 
 namespace rgw::sal::sfs {
 
+std::filesystem::path Object::get_storage_path() const {
+  return path.to_path() / std::to_string(version_id);
+}
+
+void Object::metadata_init(SFStore *store, const std::string & bucket_name,
+                           bool new_object, bool new_version) {
+  sqlite::DBOPObjectInfo oinfo;
+  oinfo.uuid = path.get_uuid();
+  oinfo.bucket_name = bucket_name;
+  oinfo.name = name;
+
+  if (new_object) {
+    sqlite::SQLiteObjects dbobjs(store->db_conn);
+    dbobjs.store_object(oinfo);
+  }
+
+  if (new_version) {
+    sqlite::DBOPVersionedObjectInfo version_info;
+    version_info.object_id = path.get_uuid();
+    version_info.object_state = ObjectState::OPEN;
+    version_info.version_id = instance;
+    sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+    version_id = db_versioned_objs.insert_versioned_object(version_info);
+  }
+}
+
+void Object::metadata_change_version_state(SFStore *store, ObjectState state) {
+  sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+  auto versioned_object = db_versioned_objs.get_versioned_object(version_id);
+  ceph_assert(versioned_object.has_value());
+  versioned_object->object_state = state;
+  if (state == ObjectState::DELETED) {
+    deleted = true;
+    versioned_object->deletion_time = ceph::real_clock::now();
+  }
+  db_versioned_objs.store_versioned_object(*versioned_object);
+}
+
+void Object::metadata_finish(SFStore *store) {
+  sqlite::SQLiteObjects dbobjs(store->db_conn);
+  auto db_object = dbobjs.get_object(path.get_uuid());
+  ceph_assert(db_object.has_value());
+  db_object->size = meta.size;
+  db_object->etag = meta.etag;
+  db_object->mtime = meta.mtime;
+  db_object->set_mtime = meta.set_mtime;
+  db_object->delete_at = meta.delete_at;
+  db_object->attrs = meta.attrs;
+  dbobjs.store_object(*db_object);
+
+  sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+  auto db_versioned_object = db_versioned_objs.get_versioned_object(version_id);
+  ceph_assert(db_versioned_object.has_value());
+  // TODO calculate checksum. Is it already calculated while writing?
+  db_versioned_object->size = meta.size;
+  db_versioned_object->creation_time = meta.mtime;
+  db_versioned_object->object_state = ObjectState::COMMITTED;
+  db_versioned_objs.store_versioned_object(*db_versioned_object);
+}
+
+ObjectRef Bucket::get_or_create(const rgw_obj_key &key) {
+  std::lock_guard l(obj_map_lock);
+  ObjectRef obj = nullptr;
+  auto new_object = true;
+  auto create_new_version = true;
+  auto it = objects.find(key.name);
+  if (it != objects.end()) {
+    obj = it->second;
+    new_object = false;
+    if (key.instance.empty() || key.instance == obj->instance) {
+      create_new_version = false;
+    } else {
+      obj->instance = key.instance;
+    }
+  } else {
+    obj = std::make_shared<Object>(key);
+    objects[key.name] = obj;
+  }
+  obj->metadata_init(store, name, new_object, create_new_version);
+
+  return obj;
+}
 
 void Bucket::finish(const DoutPrefixProvider *dpp, const std::string &objname) {
   std::lock_guard l(obj_map_lock);
 
-  auto it = creating.find(objname);
-  if (it == creating.end()) {
-    return;
-  }
 
   // finished creating the object
-  ceph_assert(objects.count(objname) == 0);
-  objects[objname] = creating[objname];
-  creating.erase(objname);
 
   ObjectRef ref = objects[objname];
   sqlite::DBOPObjectInfo oinfo;
@@ -52,10 +129,38 @@ void Bucket::finish(const DoutPrefixProvider *dpp, const std::string &objname) {
   dbobjs.store_object(oinfo);
 }
 
+void Bucket::delete_object(ObjectRef objref) {
+  std::lock_guard l(obj_map_lock);
+
+  sqlite::SQLiteVersionedObjects db_versioned_objs(store->db_conn);
+  // get the last available version to make a copy changing the object state to DELETED
+  auto last_version = db_versioned_objs.get_last_versioned_object(objref->path.get_uuid());
+  ceph_assert(last_version.has_value());
+  last_version->object_state = ObjectState::DELETED;
+  last_version->deletion_time = ceph::real_clock::now();
+
+  if (last_version->version_id != "") {
+    // generate a new version id
+    #define OBJ_INSTANCE_LEN 32
+    char buf[OBJ_INSTANCE_LEN + 1];
+    gen_rand_alphanumeric_no_underscore(store->ceph_context(), buf, OBJ_INSTANCE_LEN);
+    last_version->version_id = std::string(buf);
+    objref->instance = last_version->version_id;
+    // insert a new deleted version
+    db_versioned_objs.insert_versioned_object(*last_version);
+  } else {
+    db_versioned_objs.store_versioned_object(*last_version);
+  }
+  objref->deleted = true;
+}
+
 void Bucket::_refresh_objects() {
   sqlite::SQLiteObjects objs(store->db_conn);
   auto existing = objs.get_objects(name);
   for (const auto &obj : existing) {
+    sqlite::SQLiteVersionedObjects objs_versions(store->db_conn);
+    auto last_version = objs_versions.get_last_versioned_object(obj.uuid);
+    ceph_assert(last_version.has_value());
     ObjectRef ref = std::make_shared<Object>(obj.name, obj.uuid, false);
     ref->meta.size = obj.size;
     ref->meta.etag = obj.etag;
@@ -63,6 +168,10 @@ void Bucket::_refresh_objects() {
     ref->meta.set_mtime = obj.set_mtime;
     ref->meta.delete_at = obj.delete_at;
     ref->meta.attrs = obj.attrs;
+    ref->version_id = last_version->id;
+    ref->instance = last_version->version_id;
+    ref->deleted = (last_version->object_state == ObjectState::DELETED);
+
     objects[obj.name] = ref;
   }
 }