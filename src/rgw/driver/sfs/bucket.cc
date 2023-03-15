wipwd/s3gw-ceph

rgw/sfs: ACL implementation
Implemented persistence for sfs bucket's ACL.
ACL is represented as an attribute with key: RGW_ATTR_ACL in attrs.
This means that an implementation for the field attrs must be provided
for sfs's buckets.

Details:

 - Added battrs field to DBOPBucketInfo
 - Implemented a google test ensuring proper encode/decode of battrs.
   Same test also ensures fetching/storing from/to sqlite table.
 - Implemented SFSBucket::set_acl()
 - Implemented SFSBucket::merge_and_store_attrs()
 - Some refactoring for rgw::sal::sfs::Bucket
    - Introduced more generic instance variables:
       - RGWBucketInfo info
       - rgw::sal::Attrs attrs
    - Removed instance variables: name, bucket, creation_time, placement_rule, flags.
    - Removed to_rgw_bucket_info() methods.
- Added test/rgw/sfs/test_rgw_sfs_sqlite_buckets.cc for testing SFSBucket class
    - Added test: UserCreateBucketCheckGotFromCreate
    - Added test: UserCreateBucketCheckGotFromStore
    - Added test: BucketSetAcl
    - Added test: BucketMergeAndStoreAttrs

Fixes: https://github.com/aquarist-labs/s3gw/issues/5
Signed-off-by: Giuseppe Baccini <giuseppe.baccini@suse.com>

src/rgw/store/sfs/bucket.cc
@@ -24,9 +24,15 @@ namespace rgw::sal {
 
 
 SFSBucket::SFSBucket(
-  SFStore *_store, sfs::BucketRef _bucket, const RGWBucketInfo &info
-) : store(_store), bucket(_bucket), acls() {
-  this->info = info;
+  SFStore *_store, sfs::BucketRef _bucket
+) : Bucket(_bucket->get_info()), store(_store), bucket(_bucket) {
+  set_attrs(bucket->get_attrs());
+
+  auto it = attrs.find(RGW_ATTR_ACL);
+  if(it != attrs.end()){
+    auto lval = it->second.cbegin();
+    acls.decode(lval);
+  }
 }
 
 void SFSBucket::write_meta(const DoutPrefixProvider *dpp) {
@@ -157,6 +163,22 @@ int SFSBucket::load_bucket(
   return 0;
 }
 
+int SFSBucket::set_acl(const DoutPrefixProvider *dpp,
+                       RGWAccessControlPolicy &acl,
+                       optional_yield y) {
+    acls = acl;
+
+    bufferlist aclp_bl;
+    acls.encode(aclp_bl);
+    attrs[RGW_ATTR_ACL] = aclp_bl;
+
+    sfs::get_meta_buckets(get_store().db_conn)->store_bucket(
+      sfs::sqlite::DBOPBucketInfo(get_info(), get_attrs()));
+
+    store->_refresh_buckets_safe();
+    return 0;
+}
+
 int SFSBucket::chown(const DoutPrefixProvider *dpp, User *new_user,
                             User *old_user, optional_yield y,
                             const std::string *marker) {
@@ -185,12 +207,22 @@ int SFSBucket::check_empty(const DoutPrefixProvider *dpp,
 }
 
 int SFSBucket::merge_and_store_attrs(const DoutPrefixProvider *dpp,
-                                            Attrs &new_attrs,
-                                            optional_yield y) {
-  /** Set the attributes in attrs, leaving any other existing attrs set, and
-   * write them to the backing store; a merge operation */
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+                                     Attrs &new_attrs,
+                                     optional_yield y) {
+    for(auto& it : new_attrs) {
+	    attrs[it.first] = it.second;
+
+      if(it.first == RGW_ATTR_ACL){
+        auto lval = it.second.cbegin();
+        acls.decode(lval);
+      }
+    }
+
+    sfs::get_meta_buckets(get_store().db_conn)->store_bucket(
+      sfs::sqlite::DBOPBucketInfo(get_info(), get_attrs()));
+
+    store->_refresh_buckets_safe();
+    return 0;
 }
 
 std::unique_ptr<MultipartUpload> SFSBucket::get_multipart_upload(
@@ -301,13 +333,13 @@ int SFSBucket::check_bucket_shards(const DoutPrefixProvider *dpp) {
   ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
   return -ENOTSUP;
 }
-int SFSBucket::put_info(const DoutPrefixProvider *dpp, bool exclusive,
-                               ceph::real_time mtime) {
-  sfs::sqlite::DBOPBucketInfo store_info;
-  store_info.binfo = info;
+int SFSBucket::put_info(const DoutPrefixProvider *dpp,
+                        bool exclusive,
+                        ceph::real_time mtime) {
 
-  auto meta_buckets = sfs::get_meta_buckets(store->db_conn);
-  meta_buckets->store_bucket(store_info);
+  sfs::get_meta_buckets(get_store().db_conn)->store_bucket(
+    sfs::sqlite::DBOPBucketInfo(get_info(), get_attrs()));
+
   store->_refresh_buckets_safe();
   return 0;
 }

irq0/ceph

src/rgw/driver/sfs/bucket.cc
@@ -24,9 +24,15 @@ namespace rgw::sal {
 
 
 SFSBucket::SFSBucket(
-  SFStore *_store, sfs::BucketRef _bucket, const RGWBucketInfo &info
-) : store(_store), bucket(_bucket), acls() {
-  this->info = info;
+  SFStore *_store, sfs::BucketRef _bucket
+) : StoreBucket(_bucket->get_info()), store(_store), bucket(_bucket) {
+  set_attrs(bucket->get_attrs());
+
+  auto it = attrs.find(RGW_ATTR_ACL);
+  if(it != attrs.end()){
+    auto lval = it->second.cbegin();
+    acls.decode(lval);
+  }
 }
 
 void SFSBucket::write_meta(const DoutPrefixProvider *dpp) {
@@ -157,15 +163,33 @@ int SFSBucket::load_bucket(
   return 0;
 }
 
+int SFSBucket::set_acl(const DoutPrefixProvider *dpp,
+                       RGWAccessControlPolicy &acl,
+                       optional_yield y) {
+    acls = acl;
+
+    bufferlist aclp_bl;
+    acls.encode(aclp_bl);
+    attrs[RGW_ATTR_ACL] = aclp_bl;
+
+    sfs::get_meta_buckets(get_store().db_conn)->store_bucket(
+      sfs::sqlite::DBOPBucketInfo(get_info(), get_attrs()));
+
+    store->_refresh_buckets_safe();
+    return 0;
+}
+
 int SFSBucket::chown(const DoutPrefixProvider *dpp, User &new_user,
                             optional_yield y) {
   ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
   return -ENOTSUP;
 }
+
 bool SFSBucket::is_owner(User *user) {
   ldout(store->ceph_context(), 10) << __func__ << ": TODO" << dendl;
   return true;
 }
+
 int SFSBucket::check_empty(const DoutPrefixProvider *dpp,
                                   optional_yield y) {
   /** Check in the backing store if this bucket is empty */
@@ -184,12 +208,22 @@ int SFSBucket::check_empty(const DoutPrefixProvider *dpp,
 }
 
 int SFSBucket::merge_and_store_attrs(const DoutPrefixProvider *dpp,
-                                            Attrs &new_attrs,
-                                            optional_yield y) {
-  /** Set the attributes in attrs, leaving any other existing attrs set, and
-   * write them to the backing store; a merge operation */
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+                                     Attrs &new_attrs,
+                                     optional_yield y) {
+    for(auto& it : new_attrs) {
+	    attrs[it.first] = it.second;
+
+      if(it.first == RGW_ATTR_ACL){
+        auto lval = it.second.cbegin();
+        acls.decode(lval);
+      }
+    }
+
+    sfs::get_meta_buckets(get_store().db_conn)->store_bucket(
+      sfs::sqlite::DBOPBucketInfo(get_info(), get_attrs()));
+
+    store->_refresh_buckets_safe();
+    return 0;
 }
 
 std::unique_ptr<MultipartUpload> SFSBucket::get_multipart_upload(
@@ -300,13 +334,13 @@ int SFSBucket::check_bucket_shards(const DoutPrefixProvider *dpp) {
   ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
   return -ENOTSUP;
 }
-int SFSBucket::put_info(const DoutPrefixProvider *dpp, bool exclusive,
-                               ceph::real_time mtime) {
-  sfs::sqlite::DBOPBucketInfo store_info;
-  store_info.binfo = info;
+int SFSBucket::put_info(const DoutPrefixProvider *dpp,
+                        bool exclusive,
+                        ceph::real_time mtime) {
 
-  auto meta_buckets = sfs::get_meta_buckets(store->db_conn);
-  meta_buckets->store_bucket(store_info);
+  sfs::get_meta_buckets(get_store().db_conn)->store_bucket(
+    sfs::sqlite::DBOPBucketInfo(get_info(), get_attrs()));
+
   store->_refresh_buckets_safe();
   return 0;
 }