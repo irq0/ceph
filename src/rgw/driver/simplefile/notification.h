wipwd/s3gw-ceph

rgw/sfs: add stub notifications
Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/store/simplefile/notification.h
@@ -1,0 +1,54 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+#define RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+
+#include "rgw_sal.h"
+
+namespace rgw::sal {
+
+class SimpleFileNotification : public Notification {
+
+public:
+
+  SimpleFileNotification(
+    Object *obj,
+    Object *src_obj,
+    rgw::notify::EventType type
+  ) : Notification(obj, src_obj, type) { }
+
+  ~SimpleFileNotification() = default;
+
+  virtual int publish_reserve(
+    const DoutPrefixProvider *dpp,
+    RGWObjTags *obj_tags = nullptr
+  ) override {
+    return 0;
+  }
+
+  virtual int publish_commit(
+    const DoutPrefixProvider *dpp,
+    uint64_t size,
+    const ceph::real_time &mtime,
+    const std::string &etag,
+    const std::string &version
+  ) override {
+    return 0;
+  }
+
+};
+
+} // ns rgw::sal
+
+#endif // RGW_STORE_SIMPLEFILE_NOTIFICATION_H

irq0/ceph

src/rgw/driver/simplefile/notification.h
@@ -1,0 +1,55 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#ifndef RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+#define RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+
+#include "rgw_sal.h"
+#include "rgw_sal_store.h"
+
+namespace rgw::sal {
+
+class SimpleFileNotification : public StoreNotification {
+
+public:
+
+  SimpleFileNotification(
+    Object *obj,
+    Object *src_obj,
+    rgw::notify::EventType type
+  ) : StoreNotification(obj, src_obj, type) { }
+
+  ~SimpleFileNotification() = default;
+
+  virtual int publish_reserve(
+    const DoutPrefixProvider *dpp,
+    RGWObjTags *obj_tags = nullptr
+  ) override {
+    return 0;
+  }
+
+  virtual int publish_commit(
+    const DoutPrefixProvider *dpp,
+    uint64_t size,
+    const ceph::real_time &mtime,
+    const std::string &etag,
+    const std::string &version
+  ) override {
+    return 0;
+  }
+
+};
+
+} // ns rgw::sal
+
+#endif // RGW_STORE_SIMPLEFILE_NOTIFICATION_H