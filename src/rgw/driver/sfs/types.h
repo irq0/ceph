wipwd/s3gw-ceph

rgw/sfs: undelete objects implementation.
This allows the user to remove delete makers of objects which means
basically undeleting objects.

It also shows delete markers correctly, so users are able to find
what objects have been deleted.

Fixes: https://github.com/aquarist-labs/s3gw/issues/107
Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>

src/rgw/store/sfs/types.h
@@ -27,6 +27,7 @@
 #include "rgw/store/sfs/sqlite/dbconn.h"
 #include "rgw/store/sfs/sqlite/sqlite_buckets.h"
 #include "rgw/store/sfs/sqlite/sqlite_objects.h"
+#include "rgw/store/sfs/sqlite/sqlite_versioned_objects.h"
 
 namespace rgw::sal::sfs {
 
@@ -90,6 +91,9 @@ class Bucket {
 
  private:
   void _refresh_objects();
+  void _undelete_object(ObjectRef objref, const rgw_obj_key & key,
+                        sqlite::SQLiteVersionedObjects & sqlite_versioned_objects,
+                        sqlite::DBOPVersionedObjectInfo & last_version);
 
  public:
   Bucket(
@@ -155,7 +159,7 @@ class Bucket {
 
   void finish(const DoutPrefixProvider *dpp, const std::string &objname);
 
-  void delete_object(ObjectRef objref);
+  void delete_object(ObjectRef objref, const rgw_obj_key & key);
 
   inline std::string get_cls_name() { return "sfs::bucket"; }
 };

irq0/ceph

src/rgw/driver/sfs/types.h
@@ -27,6 +27,7 @@
 #include "rgw/driver/sfs/sqlite/dbconn.h"
 #include "rgw/driver/sfs/sqlite/sqlite_buckets.h"
 #include "rgw/driver/sfs/sqlite/sqlite_objects.h"
+#include "rgw/driver/sfs/sqlite/sqlite_versioned_objects.h"
 
 namespace rgw::sal::sfs {
 
@@ -90,6 +91,9 @@ class Bucket {
 
  private:
   void _refresh_objects();
+  void _undelete_object(ObjectRef objref, const rgw_obj_key & key,
+                        sqlite::SQLiteVersionedObjects & sqlite_versioned_objects,
+                        sqlite::DBOPVersionedObjectInfo & last_version);
 
  public:
   Bucket(
@@ -161,7 +165,7 @@ class Bucket {
 
   void finish(const DoutPrefixProvider *dpp, const std::string &objname);
 
-  void delete_object(ObjectRef objref);
+  void delete_object(ObjectRef objref, const rgw_obj_key & key);
 
   inline std::string get_cls_name() { return "sfs::bucket"; }
 };