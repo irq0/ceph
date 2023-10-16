ddd41db6fb45f3308f9ad9bb6abb5986c4d44b39
Marcel Lauhoff <marcel.lauhoff@suse.com>
2023-10-16 16:16:14 +0200

    rgw/sfs: Remove unused SFSObject constructor

--- src/rgw/driver/sfs/object.h
+++ src/rgw/driver/sfs/object.h
@@ -107,16 +107,6 @@ class SFSObject : public StoreObject {
       refresh_meta();
     }
   }
-  SFSObject(
-      SFStore* _st, const rgw_obj_key& _k, Bucket* _b,
-      sfs::BucketRef _bucketref, sfs::ObjectRef _objref
-  )
-      : StoreObject(_k, _b),
-        store(_st),
-        bucketref(_bucketref),
-        objref(_objref) {
-    _refresh_meta_from_object(objref);
-  }
 
   virtual std::unique_ptr<Object> clone() override {
     return std::unique_ptr<Object>(new SFSObject{*this});
